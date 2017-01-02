b5b95c5cc302f070548cf8dfff44f5b20024472e
Eric Ng <ericnjf@gmail.com>
2017-01-02 01:58:01 -0500

    feat(examples): add examples for test_regions

---
+++ _examples/test_region.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"time"
+
+	termbox "github.com/nsf/termbox-go"
+	tbregion "github.com/zwodahs/termbox-go-region"
+)
+
+func main() {
+	err := termbox.Init()
+	if err != nil {
+		panic(err)
+	}
+	tbregion.InitBorder()
+	defer termbox.Close()
+
+	events := make(chan termbox.Event)
+	go func() {
+		for {
+			events <- termbox.PollEvent()
+		}
+	}()
+
+	update := make(chan int)
+	go func() {
+		for {
+			update <- 1
+			time.Sleep(1 * time.Second / 3)
+		}
+	}()
+
+	termbox.SetInputMode(termbox.InputEsc)
+	termbox.Flush()
+
+	mainRegion := tbregion.NewRegion(100, 100, termbox.Cell{Ch: ' ', Fg: termbox.ColorDefault, Bg: termbox.ColorDefault})
+
+	region1 := mainRegion.NewRegion(10, 10, termbox.Cell{Ch: ' ', Fg: termbox.ColorDefault, Bg: termbox.ColorRed})
+	region1.DrawThinBorder()
+
+	region2 := region1.NewRegion(5, 5, termbox.Cell{Ch: ' ', Fg: termbox.ColorDefault, Bg: termbox.ColorBlue})
+	region2.DrawThinBorder()
+
+	selectedRegion := &region1
+
+loop:
+	for {
+		select {
+		case e := <-events:
+			switch e.Type {
+			case termbox.EventKey:
+				switch e.Key {
+				case termbox.KeyEsc:
+					break loop
+				case termbox.KeyArrowDown:
+					(*selectedRegion).SetPosition((*selectedRegion).GetPosition().Add(0, 1))
+				case termbox.KeyArrowUp:
+					(*selectedRegion).SetPosition((*selectedRegion).GetPosition().Add(0, -1))
+				case termbox.KeyArrowLeft:
+					(*selectedRegion).SetPosition((*selectedRegion).GetPosition().Add(-1, 0))
+				case termbox.KeyArrowRight:
+					(*selectedRegion).SetPosition((*selectedRegion).GetPosition().Add(1, 0))
+				default:
+					switch e.Ch {
+					case 'h':
+						region2.Hidden = !region2.Hidden
+					case '1':
+						selectedRegion = &region1
+					case '2':
+						selectedRegion = &region2
+					}
+				}
+			}
+			mainRegion.Draw()
+			termbox.Flush()
+		case <-update:
+			mainRegion.Draw()
+			termbox.Flush()
+		}
+	}
+}
